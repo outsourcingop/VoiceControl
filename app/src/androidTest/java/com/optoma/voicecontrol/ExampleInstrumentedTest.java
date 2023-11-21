eb69e55f037da3f3a3ac9042ae0caa7645dd8948
outsourcingop.ck <outsourcing.op@gmail.com>
2023-11-21 11:13:45 +0800

    Downgrade the gradle version to 6.5

--- app/src/androidTest/java/com/optoma/voicecontrol/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/optoma/voicecontrol/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.optoma.meeting", appContext.getPackageName());
+        assertEquals("com.optoma.voicecontrol", appContext.getPackageName());
     }
 }
